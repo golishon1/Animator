5486f1ffd3ec72bd254b7ca5de2043fb9bf534d0
Yaros.S <sergeyya@playtika.com>
2020-10-27 12:59:06 +0300

    added my animator and character controller

--- AnimatorTest/Assets/Standard Assets/2D/Scripts/MyController.cs
+++ AnimatorTest/Assets/Standard Assets/2D/Scripts/MyController.cs
@@ -3,15 +3,16 @@
 public class MyController : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
+    
     private Animator _animator;
-    private Rigidbody2D _rigidbody;
 
-    private int _jump = Animator.StringToHash("Jump");
+    private bool _isCrunch;
+
+    private readonly int _jump = Animator.StringToHash("Jump");
+    private Rigidbody2D _rigidbody;
 
     private Vector2 _startScale;
 
-    private bool _isCrunch;
-    
     private void Start()
     {
         _startScale = transform.localScale;
@@ -22,29 +23,39 @@ public class MyController : MonoBehaviour
     private void Update()
     {
         var h = Input.GetAxis("Horizontal");
-        _animator.SetFloat("Run", Mathf.Abs(h));
-        if (h < 0)
-            gameObject.transform.localScale = new Vector2(-_startScale.x, _startScale.y);
-        else
-            gameObject.transform.localScale = new Vector2(_startScale.x, _startScale.y);
-        _rigidbody.velocity = new Vector2(h * _speed, _rigidbody.velocity.y);
 
+        Move(h);
+        Flip(h);
+        Jump();
+        Crunch();
+    }
+
+    private void Move(float h)
+    {
+        _animator.SetFloat("Run", Mathf.Abs(h));
+        _rigidbody.velocity = new Vector2(h * _speed, _rigidbody.velocity.y);
+    }
+
+    private void Flip(float h)
+    {
+        gameObject.transform.localScale = h < 0
+            ? new Vector2(-_startScale.x, _startScale.y)
+            : new Vector2(_startScale.x, _startScale.y);
+    }
+
+    private void Jump()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
-        { 
+        {
             _rigidbody.AddForce(new Vector2(0f, 400f));
             _animator.SetTrigger(_jump);
         }
+    }
 
-        if (Input.GetKey(KeyCode.LeftControl))
-        {
-            _isCrunch = true;
-           
-        }
-        if (Input.GetKeyUp(KeyCode.LeftControl))
-        {
-            _isCrunch = false;
-           
-        }
-        _animator.SetBool("Crunch",_isCrunch);
+    private void Crunch()
+    {
+        if (Input.GetKey(KeyCode.LeftControl)) _isCrunch = true;
+        if (Input.GetKeyUp(KeyCode.LeftControl)) _isCrunch = false;
+        _animator.SetBool("Crunch", _isCrunch);
     }
 }
