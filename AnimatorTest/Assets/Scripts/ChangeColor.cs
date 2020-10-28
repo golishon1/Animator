182381240f4ae20005cf2cf5ed77a4996ad30569
Yaros.S <sergeyya@playtika.com>
2020-10-28 16:45:03 +0300

    complete Task 4

---
+++ AnimatorTest/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer _object;
+    [SerializeField] private Color _color;
+
+    private Color _startColor;
+
+    private void OnEnable()
+    {
+        MoveChangeColor.OnEnter += Enter;
+        MoveChangeColor.OnExit += Exit;
+    }
+
+    private void OnDisable()
+    {
+        MoveChangeColor.OnEnter -= Enter;
+        MoveChangeColor.OnExit -= Exit;
+    }
+
+    private void Enter()
+    {
+        _object.material.color = _color;
+    }
+
+    private void Exit()
+    {
+        _object.material.color = _startColor;
+    }
+    void Start()
+    {
+        _startColor = _object.material.color;
+    }
+
+  
+}
---
+++ AnimatorTest/Assets/Scripts/MoveChangeColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveChangeColor : StateMachineBehaviour
+{
+
+    public static event Action OnEnter;
+    public static event Action OnExit;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        OnEnter?.Invoke();
+    }
+    
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        OnExit?.Invoke();
+    }
+
+    
+}
