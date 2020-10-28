09306867dcea0dd4e0cfb8ed068c9747d7bfe7a6
Yaros.S <sergeyya@playtika.com>
2020-10-28 13:02:20 +0300

    added animator with layer and override animator clip

---
+++ AnimatorTest/Assets/Scripts/LayerAnimator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LayerAnimator : MonoBehaviour
+{
+    private readonly int _move = Animator.StringToHash("Move");
+    private readonly int _aim = Animator.StringToHash("Aim");
+    
+    
+    [SerializeField] private Animator _animator;
+    [SerializeField] private AnimationClip _shoot2;
+    [SerializeField] private AnimationClip _shoot1;
+    private AnimatorOverrideController _animatorOverrideController;
+
+    private void Start()
+    {
+        _animatorOverrideController = new AnimatorOverrideController(_animator.runtimeAnimatorController);
+        _animator.runtimeAnimatorController = _animatorOverrideController;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _animator.SetTrigger(_move);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+          
+            _animator.SetTrigger(_aim);
+            _animatorOverrideController["shoot2"] = _shoot1;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            _animatorOverrideController["shoot"] = _shoot2;
+            _animator.SetTrigger(_aim);
+        }
+        else
+        {
+            _animator.ResetTrigger(_aim);
+        }
+    }
+}
